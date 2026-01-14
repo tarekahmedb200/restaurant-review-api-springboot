031b5d8dbb1bbf2687774ee7828c53e8574efe56
Admin <admin@192.168.1.9>
2026-01-14 09:26:09 +0200

    first commit

---
+++ src/main/java/com/example/restaurant_review_api_springboot/RestaurantReviewApiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.restaurant_review_api_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantReviewApiSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantReviewApiSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restaurant_review_api_springboot/RestaurantReviewApiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restaurant_review_api_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantReviewApiSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
